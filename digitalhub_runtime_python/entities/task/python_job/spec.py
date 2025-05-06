9f1b11502e367a5b8ce8f5891e81db0dad1b50e3
EvangMM <matteomartini92@yahoo.it>
2025-05-06 13:40:53 +0200

    chore: cleanup

--- digitalhub_runtime_python/entities/task/python_job/spec.py
+++ digitalhub_runtime_python/entities/task/python_job/spec.py
@@ -4,37 +4,9 @@ from digitalhub.entities.task._base.spec import TaskSpecFunction, TaskValidatorF
 
 
 class TaskSpecPythonJob(TaskSpecFunction):
-    """TaskSpecPythonJob specifications."""
-
-    def __init__(
-        self,
-        function: str,
-        node_selector: list[dict] | None = None,
-        volumes: list[dict] | None = None,
-        resources: dict | None = None,
-        affinity: dict | None = None,
-        tolerations: list[dict] | None = None,
-        envs: list[dict] | None = None,
-        secrets: list[str] | None = None,
-        profile: str | None = None,
-        runtime_class: str | None = None,
-        priority_class: str | None = None,
-        **kwargs,
-    ) -> None:
-        super().__init__(
-            function,
-            node_selector,
-            volumes,
-            resources,
-            affinity,
-            tolerations,
-            envs,
-            secrets,
-            profile,
-            runtime_class,
-            priority_class,
-            **kwargs,
-        )
+    """
+    TaskSpecPythonJob specifications.
+    """
 
 
 class TaskValidatorPythonJob(TaskValidatorFunction):
