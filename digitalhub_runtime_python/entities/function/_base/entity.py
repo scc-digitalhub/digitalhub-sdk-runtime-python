421a2fb6d54b11676c78cd1ae842078168157b61
EvangMM <matteomartini92@yahoo.it>
2026-09-02 13:50:36 +0200

    chore: auto_build to false

--- digitalhub_runtime_python/entities/function/_base/entity.py
+++ digitalhub_runtime_python/entities/function/_base/entity.py
@@ -96,7 +96,7 @@ class FunctionBaseFunction(Function):
         wait: bool = False,
         log_info: bool = True,
         extensions: list[dict] | None = None,
-        auto_build: bool = True,
+        auto_build: bool = False,
         **kwargs,
     ):
         """
